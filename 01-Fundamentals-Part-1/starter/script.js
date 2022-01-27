f8540b08a2e5defd13c3874a739d1bfad930c7af
Manoj Kumar Chevvuri <ManojKumar.Chevvuri@albertsons.com>
2022-01-27 01:25:47 -0800

    day 1 practice

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,34 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log("jonas");
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let country = "India";
+// let continent = "Asia";
+// let population = 100000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 20);
+// console.log(typeof "jonas");
+
+// javascriptIsFun = "YES";
+// console.log(typeof javascriptIsFun);
+
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
