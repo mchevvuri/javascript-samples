7e81f6d761a623a29f78970b19ea973c3d71f590
Manoj Kumar Chevvuri <ManojKumar.Chevvuri@albertsons.com>
2022-01-28 08:21:24 -0800

    day 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -32,3 +32,18 @@
 // console.log(typeof population);
 // console.log(typeof country);
 // console.log(typeof language);
+
+
+// const markWeight = 78;
+// const markHeight = 1.69;
+// const johnWeight = 92;
+// const johnHeight = 1.95;
+
+// const markBmi = markWeight / (markHeight ** 2);
+// const johnBmi = johnWeight / (johnHeight ** 2);
+
+// console.log(markBmi, johnBmi);
+// const markHigherBMI = markBmi > johnBmi;
+
+// console.log(markHigherBMI)
+
