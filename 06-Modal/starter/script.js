04a2f1904518ed9e3b08172b487c5cb02dac87e3
Manoj Kumar Chevvuri <ManojKumar.Chevvuri@albertsons.com>
2022-05-13 14:14:59 -0700

    practice modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const modalContent = document.querySelector('.modal');
+const overlayContent = document.querySelector('.overlay');
+const closeModalButton = document.querySelector('.close-modal');
+const modalSelectorBtns = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < modalSelectorBtns.length; i++) {
+  modalSelectorBtns[i].addEventListener('click', openModal);
+}
+
+const closeModal = function () {
+  modalContent.classList.add('hidden');
+  overlayContent.classList.add('hidden');
+};
+
+closeModalButton.addEventListener('click', closeModal);
+overlayContent.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (keyPressEvent) {
+  if (
+    keyPressEvent.key === 'Escape' &&
+    !modalContent.classList.contains('hidden')
+  ) {
+    closeModal();
+  }
+});
+
+function openModal() {
+  modalContent.classList.remove('hidden');
+  overlayContent.classList.remove('hidden');
+}
